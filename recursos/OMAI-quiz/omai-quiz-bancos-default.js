ba25fbc60497bc7d3872c030db6d32d5d15445a8
Daniel Steven Moran Pizarro <dmbourbaki@gmail.com>
2026-07-19 17:23:24 -0500

    Add files via upload

--- recursos/OMAI-quiz/omai-quiz-bancos-default.js
+++ recursos/OMAI-quiz/omai-quiz-bancos-default.js
@@ -6,12 +6,14 @@ import { BANCOS_DIFERENCIAL } from "./omai-quiz-bancos-diferencial.js";
 import { BANCOS_INTEGRAL } from "./omai-quiz-bancos-integral.js";
 import { BANCOS_MULTIVARIABLE } from "./omai-quiz-bancos-multivariable.js";
 import { BANCOS_EDO } from "./omai-quiz-bancos-edo.js";
+import { BANCOS_LAPLACE } from "./omai-quiz-bancos-laplace.js";
 
 const BANCOS_DEFECTO = [
   ...BANCOS_DIFERENCIAL,
   ...BANCOS_INTEGRAL,
   ...BANCOS_MULTIVARIABLE,
   ...BANCOS_EDO,
+  ...BANCOS_LAPLACE,
 ];
 
 export { BANCOS_DEFECTO };
