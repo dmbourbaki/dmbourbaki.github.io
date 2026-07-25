6094c25847b91a018fcd889b0ec16dada24e352a
Daniel Steven Moran Pizarro <dmbourbaki@gmail.com>
2026-07-25 18:43:36 -0500

    Add files via upload

---
+++ recursos/millonario/omai-quiz-culturageneral-bancos-default.js
@@ -0,0 +1,48 @@
+// omai-quiz-culturageneral-bancos-default.js
+// Agrega todos los bancos de OMAI-Quiz Cultura General en un solo arreglo BANCOS_DEFECTO.
+
+import { BANCOS_HISTORIA_UNIVERSAL } from "./omai-quiz-culturageneral-bancos-historia-universal.js";
+import { BANCOS_GEOGRAFIA } from "./omai-quiz-culturageneral-bancos-geografia.js";
+import { BANCOS_CIENCIA_NATURALEZA } from "./omai-quiz-culturageneral-bancos-ciencia-naturaleza.js";
+import { BANCOS_ARTE_LITERATURA } from "./omai-quiz-culturageneral-bancos-arte-literatura.js";
+import { BANCOS_CINE } from "./omai-quiz-culturageneral-bancos-cine.js";
+import { BANCOS_MUSICA } from "./omai-quiz-culturageneral-bancos-musica.js";
+import { BANCOS_MITOLOGIA } from "./omai-quiz-culturageneral-bancos-mitologia.js";
+import { BANCOS_MATEMATICAS } from "./omai-quiz-culturageneral-bancos-matematicas.js";
+import { BANCOS_BIOLOGIA } from "./omai-quiz-culturageneral-bancos-biologia.js";
+import { BANCOS_QUIMICA } from "./omai-quiz-culturageneral-bancos-quimica.js";
+import { BANCOS_FISICA } from "./omai-quiz-culturageneral-bancos-fisica.js";
+import { BANCOS_TECNOLOGIA } from "./omai-quiz-culturageneral-bancos-tecnologia.js";
+import { BANCOS_DEPORTES } from "./omai-quiz-culturageneral-bancos-deportes.js";
+import { BANCOS_GASTRONOMIA } from "./omai-quiz-culturageneral-bancos-gastronomia.js";
+import { BANCOS_ANATOMIA_MEDICINA } from "./omai-quiz-culturageneral-bancos-anatomia-medicina.js";
+import { BANCOS_ASTRONOMIA } from "./omai-quiz-culturageneral-bancos-astronomia.js";
+import { BANCOS_IDIOMAS } from "./omai-quiz-culturageneral-bancos-idiomas.js";
+import { BANCOS_VIDEOJUEGOS } from "./omai-quiz-culturageneral-bancos-videojuegos.js";
+import { BANCOS_NATURALEZA_ANIMALES } from "./omai-quiz-culturageneral-bancos-naturaleza-animales.js";
+import { BANCOS_FILOSOFIA } from "./omai-quiz-culturageneral-bancos-filosofia.js";
+
+const BANCOS_DEFECTO = [
+  ...BANCOS_HISTORIA_UNIVERSAL,
+  ...BANCOS_GEOGRAFIA,
+  ...BANCOS_CIENCIA_NATURALEZA,
+  ...BANCOS_ARTE_LITERATURA,
+  ...BANCOS_CINE,
+  ...BANCOS_MUSICA,
+  ...BANCOS_MITOLOGIA,
+  ...BANCOS_MATEMATICAS,
+  ...BANCOS_BIOLOGIA,
+  ...BANCOS_QUIMICA,
+  ...BANCOS_FISICA,
+  ...BANCOS_TECNOLOGIA,
+  ...BANCOS_DEPORTES,
+  ...BANCOS_GASTRONOMIA,
+  ...BANCOS_ANATOMIA_MEDICINA,
+  ...BANCOS_ASTRONOMIA,
+  ...BANCOS_IDIOMAS,
+  ...BANCOS_VIDEOJUEGOS,
+  ...BANCOS_NATURALEZA_ANIMALES,
+  ...BANCOS_FILOSOFIA,
+];
+
+export { BANCOS_DEFECTO };
